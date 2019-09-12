a170691aec8b9b732eacc5606be73ec7e387509e
Danial <danny00b@gmail.com>
2019-09-12 10:15:36 -0700

    Implementing Chirp SDK to send and receive files

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-import numpy
-import pyaudio
+from chirpsdk import ChirpConnect, CallbackSet
+import os
+os.path.expanduser('~/.chirprc')
 
+chirp = ChirpConnect()
+chirp.start(send=True, receive=True)
+
+# Sending data
+identifier = 'hello'
+payload = bytearray([ord(ch) for ch in identifier])
+chirp.send(payload, blocking=True)
+
+
+# Receiving data
+class Callbacks(CallbackSet):
+    def on_received(self, payload, channel):
+        if payload is not None:
+            identifier = payload.decode('utf-8')
+            print('Received: ' + identifier)
+        else:
+            print('Decode failed')
+
+
+chirp.set_callbacks(Callbacks())
+
+# Stop processing data
+chirp.stop()
