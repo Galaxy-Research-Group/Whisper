1f8f2c6344589d9c4ae833870b2466ae51c9e049
Harris Beg <harris@harrisbeg.com>
2019-09-24 22:33:50 -0700

    Keypairs

--- tree/structure.py
+++ tree/structure.py
@@ -1,19 +1,47 @@
+import uuid
+
 class SonicTree:
     def __init__(self, right=None, left=None, value=None):
+        "TEMP"
         self.right = right
         self.left = left
+        ""
+        """
+        FUTURE:
+        self.nodes = nodes
+        """
         self.killswitch = False
         self.value = value
+        self.id= str(uuid.uuid4())
+        self.keypairs = {}
 
 
     def is_edge(self):
-        return self.right != None and self.left != None
+        return self.right == None and self.left == None
     
     def kill(self):
         tree = self
         while not tree.is_edge():
             tree.killswitch = True
             return tree.right.kill() and tree.left.kill()
+        return
+    
+    @staticmethod
+    def gen_key():
+        return str(uuid.uuid4()) # temp
+
+    def gen_keypair(self):
+        tree = self
+        while not tree.is_edge():
+            if tree.right != None:
+                key = SonicTree.gen_key()
+                tree.keypairs[tree.right.id] = key
+                tree.right.keypairs[tree.id] = key
+            if tree.left  != None:
+                key = SonicTree.gen_key()
+                tree.keypairs[tree.left.id] = key
+                tree.left.keypairs[tree.id] = key
+            return tree.right.gen_keypair() and tree.left.gen_keypair()
         return
 
     # Simple recursive transmittance --> Will not deactivate
@@ -44,17 +72,16 @@ class SonicTree:
             
     
     def __str__(self):
-        return "\n\tR:" + str(self.right) + "\tL: " + str(self.left) + "\tS: " + str(self.value)
+        return "\n\tR:" + str(self.right) + "\tL: " + str(self.left) + "\tS: " + str(self.value) + "\tK: " + str(self.keypairs)
 
 tree = SonicTree(
                 SonicTree(
                     SonicTree(None, None, {"hi":"2"}),
-                    SonicTree(None, None, True), False),
+                    SonicTree(None, None, False), False),
                     SonicTree(
                         SonicTree(None, None, {"hi":"5"}),
-                        SonicTree(None, None, {"hi":"6"}),
+                        SonicTree(None, None, True),
                 {"hi":"hello"}),
             {"hi":1})
-condition = lambda _: _ == True
-tree.transmit_kos(condition)
+tree.gen_keypair()
 print(tree)
